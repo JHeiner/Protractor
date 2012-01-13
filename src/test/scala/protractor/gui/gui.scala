e0d7111e081dbdc0154a3b2ef86bf5537d737e76
Jeremy Heiner <ScalaProtractor@gmail.com>
2012-01-12 21:40:50 -0500

    Fix the packages for the unit tests and the test GUI.

--- src/test/scala/protractor/gui/gui.scala
+++ src/test/scala/protractor/gui/gui.scala
@@ -28,7 +28,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-package protractor.test
+package protractor.gui
 
 import protractor._
 import java.awt.{Color,Dimension,Graphics2D}
--- src/test/scala/protractor/test/test.scala
+++ src/test/scala/protractor/test/test.scala
@@ -28,7 +28,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-package test
+package protractor.test
 
 import protractor._
 import org.specs2._
